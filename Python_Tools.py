449b44f67e48c3dc6489d9b4c1405b10798336dc
Plangloi <48372629+Plangloi@users.noreply.github.com>
2025-03-10 22:16:07 -0400

    Update Python_Tools.py
    Cursor

--- Python_Tools.py
+++ Python_Tools.py
@@ -1,101 +1,84 @@
 import os
 from tkinter import *
-from ttkbootstrap.constants import *
 import ttkbootstrap as tb
 
-
-# root = tk()
+# Create the main window
 root = tb.Window(themename="superhero")
-
-#root = TK()
-root.title("TTK Boostrap!")
+root.title("TTK Bootstrap!")
 root.iconbitmap('images/codemy.ico')
-root.geometry('500,350')
-
+root.geometry('500x350')  # Fixed geometry string format
 root.mainloop()
 
 out = 0
 
-
 # Main Menu ----------------------
-
 def affichagemenu():
     os.system('clear')
     os.system('toilet Power PY')
     print("\033[1;32m1-User Tools\033[m \n")
-    print("\033[1;32m2-Nmap\033[m \n")
+    print("\033[1;32m2-Nmap\033[m \n") 
     print("\033[1;32m3-SSH\033[m \n")
-    print("\033[1;33m(Q)Quitter[m \n")
+    print("\033[1;33m(Q)Quitter\033[m \n")
     
     choix = input()
     if choix == '1':
         Usertools()
     elif choix == '2':
         nmap1()
-    elif choix == 'q' or choix.lower() == 'quit':
-        quiter()
     elif choix == '3':
         ssh1()
-    elif choix == '4':
-        affichagemenu()
+    elif choix.lower() in ['q', 'quit']:
+        quiter()
     else:
-        print(" Mauvais Choix ")
+        print("Mauvais Choix")
         pause()
 
-#Pause -------------------------
-
-
+# Pause -------------------------
 def pause():
-    print("enter to continue! ")
-    input()
+    input("Enter to continue!")
     affichagemenu()
-    
-    
-#Verifier User est present dans /etc/passwd --------------
 
+# Verify User exists in /etc/passwd --------------
 def verifyuser():
     util = input("Nom user : ")
     if not util:
         print("Veuillez entrer un nom d'utilisateur valide")
+        pause()
         return
+        
     try:
         with open('/etc/passwd', 'r') as f:
             for line in f:
                 if line.startswith(util + ':'):
-                    print("L'utilisateur {} existe!".format(util))
+                    print(f"L'utilisateur {util} existe!")
                     pause()
                     return
-
+        print("L'utilisateur est introuvable\n")
     except FileNotFoundError:
-        pass
-    print("L'utilisateur est introvable\n")
+        print("Fichier /etc/passwd non trouvé")
     pause()
-
 
 # Print UID of User X--------------------
-
 def afficheruid():
-    util = input("Nom de lutilisateur : ")
+    util = input("Nom de l'utilisateur : ")
     os.system('clear')
-    uid = os.popen('id {}'.format(util)).read().split()[0]
-    print("L'uid de {} est : {}".format(util, uid))
+    uid = os.popen(f'id {util}').read().split()[0]
+    print(f"L'uid de {util} est : {uid}")
     pause()
 
-
-# Fin de progamme ----------------------
-
+# Program Exit ----------------------
 def quiter():
-    print(" Bye Bye! ")
-out = 1
-
+    print("Bye Bye!")
+    global out
+    out = 1
 
 # User Tools menu ---------------------
-
 def Usertools():
     os.system('clear')
     print("1-Vérifier l'existence d'un utilisateur\n")
     print("2-Connaître l'UID d'un utilisateur\n")
     print("\033[1;33m3-Main menu\033[m\n")
+    
     usert = input("Choisir 1-3 : \n")
     if usert == '1':
         verifyuser()
@@ -104,16 +87,16 @@ def Usertools():
     elif usert == '3':
         affichagemenu()
     else:
-        print("maivais choix")
+        print("Mauvais choix")
         pause()
 
 # Menu SSH --------------------------
-
 def ssh1():
     os.system('clear')
-    print(" 1-SSH install\n")
-    print(" 2-SSH On\n")
-    print("\033[1;33m3-Main menu\033[m\n ")
+    print("1-SSH install\n")
+    print("2-SSH On\n")
+    print("\033[1;33m3-Main menu\033[m\n")
+    
     ssh1choix = input()
     if ssh1choix == '1':
         os.system('apt update && apt install openssh')
@@ -122,56 +105,33 @@ def ssh1():
         print("choix 2")
         pause()
     elif ssh1choix == '3':
-        print("choix 3")
         affichagemenu()
-    else:
-        pass
-    
-    
-# Menu Nmap ------------------------
 
+# Menu Nmap ------------------------
 def nmap1():
     os.system('clear')
     print("1-Nmap install\n")
     print("2-Nmap port ouvert\n")
     print("\033[1;33m3-Main menu\033[m\n")
+    
     netstatchoix = input()
     if netstatchoix == '1':
         os.system('apt install nmap')
         pause()
     elif netstatchoix == '2':
-        target = input("target ip (default 192.168.1.1/24): ")
-        if not target:
-            target = '192.168.1.1/24'
-        os.system('nmap {}'.format(target))
+        target = input("target ip (default 192.168.1.1/24): ") or '192.168.1.1/24'
+        os.system(f'nmap {target}')
+        pause()
     elif netstatchoix == '3':
         affichagemenu()
     else:
-        print("Maivais choix")
+        print("Mauvais choix")
         pause()
-        
-        
-# Main function --------------------
 
+# Main function --------------------
 def main1():
     while out == 0:
-        os.system('clear')
         affichagemenu()
-        choix = input()
         
-        if choix == '1':
-            Usertools()
-        elif choix == '2':
-            nmap1()
-        elif choix == '3':
-            ssh1()
-        elif choix == '4':
-            nmap1()
-        elif choix.lower() == 'q' or choix.lower() == 'quit':
-            quiter()
-                        
-        else:
-            print(" Mauvais Choix ")
-            pause()
-main1()
-affichagemenu()
+if __name__ == '__main__':
+    main1()
